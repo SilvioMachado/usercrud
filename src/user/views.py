6bc375b842dc913fb21405245669b671a6e4374c
Silvio Machado <silvio.machado131@gmail.com>
2022-08-15 00:28:05 -0300

    created user blueprint with views
    holding crud logic

--- src/user/views.py
+++ src/user/views.py
@@ -1,27 +1,53 @@
+from flask import request
+
 from src.user.blueprint import blueprint
+from src.database.models import ClientCompany, CompanyBankAccount
 
 
-@blueprint.route('/')
-def hello():
-    return 'Hello World'
+@blueprint.get('/<int:company_id>')
+def company_get(company_id):
+    return ClientCompany.query.get_or_404(
+        company_id, description='Invalid Company'
+    ).serialize()
 
 
-@blueprint.get('/<int:user_id>')
-def user_get(user_id):
-    return f'user_id: {user_id}'
+@blueprint.post('/')
+def company_post():
+    ClientCompany.create_from_dict(request.json)
+    return ''
 
 
-@blueprint.post('/<int:user_id>')
-def user_post(user_id):
-    return f'user_id: {user_id}'
+@blueprint.delete('/<int:company_id>')
+def company_delete(company_id):
+    ClientCompany.delete_by_id(company_id)
+    return ''
 
 
-@blueprint.delete('/<int:user_id>')
-def user_delete(user_id):
-    return f'user_id: {user_id}'
+@blueprint.post('/<int:company_id>/bank_account')
+def add_bank_account(company_id):
+    client_company = ClientCompany.query.get_or_404(company_id)
+    bank_account = CompanyBankAccount.create_from_dict(request.json)
+
+    client_company.add_bank_account(bank_account)
+
+    return ''
 
 
-@blueprint.put('/<int:user_id>')
-def user_put(user_id):
-    return f'user_id: {user_id}'
+@blueprint.put('/<int:company_id>')
+def company_update(company_id):
+    client_company = ClientCompany.query.get_or_404(company_id)
+    client_company.update_from_dict(request.json)
+    return ''
 
+
+@blueprint.put('/account/<int:account_id>')
+def update_bank_account(account_id):
+    bank_account = CompanyBankAccount.query.get(account_id)
+    bank_account.update_from_dict(request.json)
+    return ''
+
+
+@blueprint.delete('/account/<int:account_id>')
+def delete_bank_account(account_id):
+    CompanyBankAccount.delete_by_id(account_id)
+    return ''
