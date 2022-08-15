f8ddeba03857fa6696aae003282139d5c7aa95ce
Silvio Machado <silvio.machado131@gmail.com>
2022-08-15 00:28:32 -0300

    running Flask app

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,36 @@
 from flask import Flask
-from src.user.blueprint import blueprint as user_blueprint
+
+from src.database.db import init_from_app
+from src.exceptions import InvalidException, handle_invalid_exception
 
 app = Flask(__name__)
-app.register_blueprint(user_blueprint, url_prefix='user/')
+app.config.from_pyfile('/app/config/local.py')
+
+# Database related
+init_from_app(app)
+
+# Error handling related
+app.register_error_handler(400, handle_invalid_exception)
+
+# Blueprints related
+from src.user import blueprint as user_blueprint
+app.register_blueprint(user_blueprint, url_prefix='/company')
+
+
+# Shell related
+@app.cli.command('shell_plus')
+def shell_plus():
+    from IPython import embed
+    from src.database.models import (
+        Bank, CompanyBankAccount, ClientCompany
+    )
+    from src.database.db import db
+
+    embed(
+        user_ns={
+            'app': app,
+            'Bank': Bank,
+            'CompanyBankAccount': CompanyBankAccount,
+            'ClientCompany': ClientCompany,
+            'db': db
+        })
