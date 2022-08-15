92eb93825d19673c89b9ca05d6606107e93741f0
Silvio Machado <silvio.machado131@gmail.com>
2022-08-15 00:26:10 -0300

    envs configuration and requirements to install

---
+++ config/production.py
@@ -0,0 +1,2 @@
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:local@postgres_local:5432/usercrud'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ config/test.py
@@ -0,0 +1,2 @@
+TESTING = True
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:local@postgres_local:5432/usercrud_test'
